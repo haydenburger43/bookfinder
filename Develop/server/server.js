5e97ea1b791d8ab3f4c8b9968d618333934d8296
hayden <haydenburger43@gmail.com>
2022-09-30 14:08:46 -0500

    updated schemas and server.js

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -12,6 +12,11 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+
+  server.applyMiddleware({ app });
+  
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
